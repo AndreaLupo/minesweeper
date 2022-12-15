d5b0f543e9284e4f5d8561e7f9a5e05dfd226756
Wolf Developer <lupo.andrea.93@gmail.com>
2022-12-15 18:52:13 +0100

    Improve playboard

--- src/components/game/GamePlayboard.vue
+++ src/components/game/GamePlayboard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="grid">
-    <MineModal :show="$dial.showDialog" title="You lost!"> </MineModal>
+    <MineModal :show="isGameEnded" title="You lost!"> </MineModal>
     <PlayboardCell
       v-for="cell in cellList"
       :key="cell.key"
       :cell="cell"
-      @end-game="endGame"
+      @end-game="clickOnBomb"
       @open-empty-adjacent="openEmptyAdjacent"
       @open-cells-around="openCellsAround"
     ></PlayboardCell>
@@ -14,43 +14,57 @@
 
 <script setup lang="ts">
 import { Cell, CellStatus } from "@/model/Cell";
-import { FixedGrid } from "@/model/grid/FixedGrid";
-import { Difficulty } from "@/model/grid/GameGrid";
+import { Difficulty, GameResult } from "@/model/grid/GameGrid";
 import { useGameStore } from "@/stores/match";
+import { useStatisticsStore } from "@/stores/statistics";
 import { storeToRefs } from "pinia";
-import { computed, reactive, toRef } from "vue";
+import { computed, reactive } from "vue";
 import MineModal from "../ui/MineModal.vue";
 import PlayboardCell from "./PlayboardCell.vue";
 
 const gameStore = useGameStore();
+const statisticsStore = useStatisticsStore();
 gameStore.initGrid(Difficulty.EASY);
-const { gameGrid } = storeToRefs(gameStore);
-// const gameGrid: FixedGrid = new FixedGrid(Difficulty.EASY);
-const $dial = reactive({ showDialog: false });
+const { gameGrid, gameResult } = storeToRefs(gameStore);
 
 const cssNumColumnFr = computed(function (): string {
   return `repeat(${gameGrid.value.numCol}, 1fr)`;
-});
-const cssNumRowFr = computed(function (): string {
-  return `repeat(${gameGrid.value.numRow}, 1fr)`;
 });
 
 const cellList = computed(function (): Array<Cell> {
   return gameGrid.value.cellList.flatMap((el) => el);
 });
 
-const endGame = (): void => {
-  $dial.showDialog = true;
+const isGameEnded = computed(function (): boolean {
+  return gameResult.value !== GameResult.NOT_END;
+});
+
+const clickOnBomb = (): void => {
+  gameResult.value = GameResult.LOOSE;
+  gameStore.togglePauseTimer();
+  statisticsStore.incrementLostGames();
 };
 const openEmptyAdjacent = (cell: Cell): void => {
   // gameGrid.printDebugGrid();
   gameGrid.value.automaticOpenAdjacentEmptyCell(cell);
+  /* 
+  doesnt work
+  const cellsAround: Cell[] = gameGrid.value.getCellsAround(cell);
+  for (const cellAround of cellsAround) {
+    cellAround.status = CellStatus.OPEN;
+  } */
 };
 
 const openCellsAround = (cell: Cell): void => {
-  const cells: Cell[] = gameGrid.value.getCellsAround(cell);
+  const cells: Cell[] = reactive(gameGrid.value.getCellsAround(cell));
   for (const cell of cells) {
-    cell.status = CellStatus.OPEN;
+    if (!cell.hasFlag && cell.isBomb) {
+      gameResult.value = GameResult.LOOSE;
+    } else if (!cell.hasFlag) {
+      cell.status = CellStatus.OPEN;
+    } else {
+      // cell has flag - n
+    }
   }
 };
 </script>
